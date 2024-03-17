09b2515c17d1449380bfd4aa917c2f51b13c3bad
chrishellon <chrisghellon@gmail.com>
2024-03-17 08:34:57 +0700

    Set Tyrel balance in packages

--- src/Core/Application/Solana/Queries/GetTyrelBalanceRequest.cs
+++ src/Core/Application/Solana/Queries/GetTyrelBalanceRequest.cs
@@ -1,3 +1,4 @@
+using SoapCapital.Application.Catalog.Interfaces;
 using SoapCapital.Application.Common.Exceptions;
 using SoapCapital.Application.Solana.Dto;
 using Solnet.Rpc;
@@ -16,9 +17,22 @@ public class GetTyrelBalanceRequest : IRequest<TyrelBalanceDto>
 
 internal class GetTyrelBalanceRequestHandler : IRequestHandler<GetTyrelBalanceRequest, TyrelBalanceDto>
 {
+    private IPackagesService _packagesService;
+
+    public GetTyrelBalanceRequestHandler(IPackagesService packagesService)
+    {
+        _packagesService = packagesService;
+    }
+
     public async Task<TyrelBalanceDto> Handle(GetTyrelBalanceRequest request, CancellationToken cancellationToken)
     {
         var tyrelBalanceDto = new TyrelBalanceDto();
+        
+        var tyrelPackage = await _packagesService.GetPackage(6);
+
+        var amountToOwn = 420000M;
+        if (tyrelPackage != null)
+            amountToOwn = tyrelPackage.Price;
         
         var client = ClientFactory.GetClient(Cluster.MainNet);
         var tokenAccounts = await client.GetTokenAccountsByOwnerAsync(ownerPubKey: request.PublicKey,
@@ -28,20 +42,20 @@ internal class GetTyrelBalanceRequestHandler : IRequestHandler<GetTyrelBalanceRe
             throw new CustomException(tokenAccounts.Reason, tokenAccounts.ErrorData.Logs.ToList(), tokenAccounts.HttpStatusCode);
 
         if (tokenAccounts.Result.Value.Count == 0)
-            throw new NotFoundException("No Tyrel token founds in this wallet. You must have at least 420,000 $tyrel to sign up.");
+            throw new NotFoundException($"No Tyrel token founds in this wallet. You must have at least {amountToOwn:N0} $tyrel to sign up.");
 
         var tyrelAccount = tokenAccounts.Result.Value.FirstOrDefault(x => x.Account.Owner == "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
         
         if (tyrelAccount == null)
-            throw new NotFoundException("No Tyrel token founds in this wallet. You must have at least 420,000 $tyrel to sign up.");
+            throw new NotFoundException($"No Tyrel token founds in this wallet. You must have at least {amountToOwn:N0} $tyrel to sign up.");
 
         var tyrelBalance = tyrelAccount.Account.Data.Parsed.Info.TokenAmount;
 
         if (tyrelBalance == null)
             return tyrelBalanceDto;
 
-        if (tyrelBalance.AmountDecimal < 42000)
-            throw new NotFoundException("You must have at least 420,000 $tyrel to sign up.");
+        if (tyrelBalance.AmountDecimal <= amountToOwn)
+            throw new NotFoundException($"You must have at least {amountToOwn:N0} $tyrel to sign up.");
         
         tyrelBalanceDto.Amount = tyrelBalance.AmountDecimal.ToString("N0");
         tyrelBalanceDto.Success = true;
